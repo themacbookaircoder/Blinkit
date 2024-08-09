7db20986508d660cf7a41790cac11341dd61b423
Kuldeep <kuldipaavashist@gmail.com>
2024-08-09 21:53:21 +0530

    initiated the xcode project and added git

---
+++ Blinkit/BlinkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkitApp.swift
+//  Blinkit
+//
+//  Created by Apple on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct BlinkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blinkit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Blinkit
+//
+//  Created by Apple on 09/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
